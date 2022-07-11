8352c1f57b1ba0724b6b4313d5b0e1df16a47be7
JIshanRandika <jishanrandika@gmail.com>
2022-07-11 19:21:09 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,65 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import type {Node} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+  Button,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+import {GoogleSignin} from '@react-native-google-signin/google-signin';
+import auth from '@react-native-firebase/auth';
+
+GoogleSignin.configure({
+  webClientId:
+    '428081873300-2ckd77rugng5tt420b1k2q7pejrs3p2v.apps.googleusercontent.com',
+});
+
+async function onGoogleButtonPress() {
+  // Get the users ID token
+  const {idToken} = await GoogleSignin.signIn();
+
+  // Create a Google credential with the token
+  const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+
+  // Sign-in the user with the credential
+  const user_sign_in = auth().signInWithCredential(googleCredential);
+
+  user_sign_in.then(user => {
+    // console.log(user);
+    console.log(user.additionalUserInfo.profile);
+  });
+}
+
+const App: () => Node = () => {
+  return (
+    <Button
+      title="Google Sign-In"
+      onPress={() =>
+        onGoogleButtonPress().then(() => console.log('Signed in with Google!'))
+      }
+    />
+  );
+};
+
+export default App;
